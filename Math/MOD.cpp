414da3ce8d58c20c960679203875457ae1c5290f
CoderVivek92 <144176643+Vivek9214@users.noreply.github.com>
2024-09-30 13:56:28 +0530

    Add files via upload

---
+++ Math/MOD.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, d,r;
+    cout<<"Enter the n:";
+    cin>>n;
+    cout<<"Enter The d : ";
+    cin>>d;
+    r=n%r;
+    cout<<r;
+    return 0;
+}
